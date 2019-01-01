923fd72cfbc0cec336d9fd7cea5da3f02acfd1c3
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-01-01 17:41:31 -0500

    Don't error out if `PYCORS_HOME` does not exists when loading settings

--- src/settings.rs
+++ src/settings.rs
@@ -4,7 +4,7 @@ use std::{
     path::PathBuf,
 };
 
-use log::{debug, error};
+use log::{debug, error, warn};
 use semver::Version;
 
 use crate::{utils, Result};
@@ -27,45 +27,56 @@ impl Settings {
         let mut installed_python = Vec::new();
         debug!("install_dir: {}", install_dir.display());
 
-        for dir in fs::read_dir(&install_dir)? {
-            match dir {
-                Ok(dir) => {
-                    let location = dir.path();
-                    let version_file_path = location.join("version");
-                    debug!("Loading version from {}", version_file_path.display());
-                    let input = match File::open(&version_file_path) {
-                        Err(e) => {
-                            error!("Error opening file {:?}: {:?}", version_file_path, e);
-                            continue;
-                        }
-                        Ok(input) => input,
-                    };
-                    let mut buffered = BufReader::new(input);
+        match fs::read_dir(&install_dir) {
+            Ok(dirs) => {
+                for dir in dirs {
+                    match dir {
+                        Ok(dir) => {
+                            let location = dir.path();
+                            let version_file_path = location.join("version");
+                            debug!("Loading version from {}", version_file_path.display());
+                            let input = match File::open(&version_file_path) {
+                                Err(e) => {
+                                    error!("Error opening file {:?}: {:?}", version_file_path, e);
+                                    continue;
+                                }
+                                Ok(input) => input,
+                            };
+                            let mut buffered = BufReader::new(input);
 
-                    let mut buffer = String::new();
-                    match buffered.read_to_string(&mut buffer) {
-                        Err(e) => {
-                            error!("Error reading file {:?}: {:?}", version_file_path, e);
-                            continue;
+                            let mut buffer = String::new();
+                            match buffered.read_to_string(&mut buffer) {
+                                Err(e) => {
+                                    error!("Error reading file {:?}: {:?}", version_file_path, e);
+                                    continue;
+                                }
+                                Ok(_) => {}
+                            }
+
+                            let version = match Version::parse(buffer.trim()) {
+                                Err(e) => {
+                                    error!(
+                                        "Error parsing version string {:?}: {:?}",
+                                        buffer.trim(),
+                                        e
+                                    );
+                                    continue;
+                                }
+                                Ok(version) => version,
+                            };
+
+                            installed_python.push(PythonVersion { location, version });
                         }
-                        Ok(_) => {}
+                        Err(e) => {
+                            error!("Error listing directory: {:?}", e);
+                        }
                     }
-
-                    let version = match Version::parse(buffer.trim()) {
-                        Err(e) => {
-                            error!("Error parsing version string {:?}: {:?}", buffer.trim(), e);
-                            continue;
-                        }
-                        Ok(version) => version,
-                    };
-
-                    installed_python.push(PythonVersion { location, version });
-                }
-                Err(e) => {
-                    error!("Error listing directory: {:?}", e);
                 }
             }
-        }
+            Err(e) => {
+                warn!("Error parsing version string {:?}: {:?}", install_dir, e);
+            }
+        };
 
         Ok(Settings { installed_python })
     }
