37e29c59770afa53e71294617c498bbd94de48fa
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-11-27 08:53:10 -0500

    Log at end of install

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -13,7 +13,7 @@ use crate::{
         self,
         install::{download::download_source, pip::install_extra_pip_packages},
     },
-    constants::TOOLCHAIN_FILE,
+    constants::{EXECUTABLE_NAME, TOOLCHAIN_FILE},
     toolchain::{find_installed_toolchains, installed::InstalledToolchain, ToolchainFile},
     utils, Result,
 };
@@ -130,6 +130,24 @@ pub fn run(
             &requested_version.version,
             install_extra_packages,
         )?;
+    }
+
+    log::info!("Installing {} succeeded!", requested_version.version);
+    if select {
+        log::info!(
+            "Version {} is selected and will be used in current directory.",
+            requested_version.version
+        );
+    } else {
+        log::info!(
+            "Version {} was installed but is not selected. Select it with:",
+            requested_version.version
+        );
+        log::info!(
+            "    {} select {}",
+            EXECUTABLE_NAME,
+            VersionReq::exact(&requested_version.version)
+        );
     }
 
     Ok(())
