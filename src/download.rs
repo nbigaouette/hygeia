784bdd637d28c27c57e9dcf9dee3b4655af3d8ba
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-12-02 21:55:07 -0500

    Add back the download progress bar

--- src/download.rs
+++ src/download.rs
@@ -58,8 +58,6 @@ async fn _download_to_path(url: &str, download_to: &Path) -> Result<()> {
         println!("skipped: file {} already downloaded.", filename);
         Ok(())
     } else {
-        log::info!("Downloading {}...", url);
-
         let downloaded_data = download(&url).await?;
 
         let mut output = BufWriter::new(File::create(&file_path)?);
@@ -85,21 +83,48 @@ async fn download(url: &Url) -> Result<Vec<u8>> {
         .authority(url.host_str().unwrap())
         .path_and_query(url.path())
         .build()?;
-    log::debug!("uri: {}...", uri);
+    let filename: &str = match url
+        .path_segments()
+        .ok_or_else(|| anyhow::anyhow!("cannot extract path segments from {:?}", url))?
+        .last()
+        .ok_or_else(|| anyhow::anyhow!("cannot extract filename from {:?}", url))? {
+            "" => url.as_str(),
+            filename @ _ => filename,
+        };
 
     let mut response = client.get(uri).await?;
 
     if !response.status().is_success() {
         anyhow::bail!("Failed to query python.org: {:?}", response);
     }
+
+    // Create a progress bar
+    let headers = response.headers().clone();
+    let ct_len = match headers.get(hyper::header::CONTENT_LENGTH).cloned() {
+        Some(ct_len) => {
+            let ct_len: u64 = ct_len.to_str()?.parse()?;
+            log::debug!("Downloading {} bytes...", ct_len);
+            Some(ct_len)
+        }
+        None => {
+            log::warn!("Could not find out file size");
+            None
+        }
+    };
+
+    let message = format!("Downloading {:?}...", filename);
+    let pb = create_progress_bar(&message, ct_len);
+
     let body = response.body_mut();
 
-    // FIXME: Send updates to indicatif (progress bar)
     let mut output: Vec<u8> = Vec::with_capacity(1024);
     while let Some(chunk) = body.next().await {
         let bytes = chunk?.into_bytes();
+        pb.inc(bytes.len() as u64);
         output.write_all(&bytes[..])?;
     }
+
+    pb.finish();
 
     Ok(output)
 }
@@ -122,4 +147,23 @@ fn build_url(version: &Version) -> Result<Url> {
     ))?;
 
     Ok(to_download)
+}
+
+fn create_progress_bar(msg: &str, length: Option<u64>) -> ProgressBar {
+    let pb = match length {
+        Some(len) => ProgressBar::new(len),
+        None => ProgressBar::new_spinner(),
+    };
+
+    pb.set_message(msg);
+    if length.is_some() {
+        pb
+            .set_style(ProgressStyle::default_bar()
+                .template("{spinner:.green} {msg} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} eta: {eta}")
+                .progress_chars("=> "));
+    } else {
+        pb.set_style(ProgressStyle::default_spinner());
+    }
+
+    pb
 }
