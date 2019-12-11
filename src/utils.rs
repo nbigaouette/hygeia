c072156dc179366ec980b834e91dea9768e4ed4e
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-12-10 21:31:35 -0500

    CI is failing to match regex; print file content to debug
    See https://github.com/nbigaouette/pycors/runs/343098568

--- src/utils.rs
+++ src/utils.rs
@@ -673,6 +673,7 @@ mod tests {
 
         let re = regex::Regex::new(r#"(?P<date>20[0-9]{2}-[0-3][0-9]-[0-1][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9]{6}[-+][0-2][0-9]:[0-5][0-9]) - (?P<cmd>.*)"#).unwrap();
         let file_content = fs::read_to_string(&expected_file_path).unwrap();
+        println!("file_content:\n{}", file_content);
 
         let lines: Vec<&str> = file_content.lines().collect();
 
@@ -705,6 +706,7 @@ mod tests {
 
         let re = regex::Regex::new(r#"(?P<date>20[0-9]{2}-[0-3][0-9]-[0-1][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9]{6}[-+][0-2][0-9]:[0-5][0-9]) - (?P<cmd>.*)"#).unwrap();
         let file_content = fs::read_to_string(&expected_file_path).unwrap();
+        println!("file_content:\n{}", file_content);
 
         let lines: Vec<&str> = file_content.lines().collect();
 
